c0481fc2251cfafbea43cffaaad8c8f945e7f201
Yannick Warnier <ywarnier@beeznest.org>
2024-06-11 11:36:25 +0200

    Update ignored config

--- config/config.dist.php
+++ config/config.dist.php
@@ -23,10 +23,10 @@ $defaultStaffRoleId = 3;
 $defaultStaffLanguage = '{"browser_lang":"fr"}';
 $defaultStaffPermissions = '{"user.create":1,"user.delete":1,"user.edit":1,"user.manage":1,"user.dir":1,"org.create":1,"org.delete":1,"org.edit":1,"faq.manage":1,"visibility.agents":1,"emails.banlist":1,"visibility.departments":1}';
 
+// @todo fill the following arrays via select queries
 // Array of IDs of each user field in ost_user__cdata.
 // These can be found in ost_form_field with form_id = 1 and label or name to get the translation
-// Todo: fill via a select query
-// Prod
+// The IDs come from the fields created (or pre-existing) in the User creation form in OsTicket
 $fieldsList = [
     'clientnum' => 79,
     'title' => 73,
@@ -41,55 +41,28 @@ $fieldsList = [
     'tag' => 75,
     'recommendedby' => 77,
 ];
-// Test
-$fieldsList = [
-    'clientnum' => 38,
-    'title' => 39,
-    'firstname' => 40,
-    'mobilephone' => 41,
-    'landphone' => 42,
-    'address' => 43,
-    'postcode' => 44,
-    'city' => 45,
-    'lang' => 46,
-    'email2' => 47,
-    'tag' => 48,
-    'recommendedby' => 49,
-];
-
-// Prod
+// Translate languages in OTRS to language IDs in OsTicket
+// The IDs come from a "list" of values created manually in OsTicket
 $langTranslation = [
     'en' => '26',
     'fr' => '24',
     'nl' => '25',
     '-' => '27'
 ];
-// Test
-$langTranslation = [
-    'en' => '5',
-    'fr' => '6',
-    'nl' => '7',
-    '-' => '4'
-];
-
+// Literal strings for language shortcodes
+// "-" serves when no language was selected
+// These come from a "list" of values created manually in OsTicket
 $langTranslationTerms = [
     'en' => 'English',
     'fr' => 'Fran\u00e7ais',
     'nl' => 'Nederlands',
     '-' => 'Autre'
 ];
-
-// Prod
+// IDs (in OsTicket) for "titles" obtained from OTRS
+// The IDs come from the values of a list created manually in OsTicket
 $titleTranslation = [
     'Mr' => 32,
     'Mme' => 31,
-    'Mlle' => 42,
-    '-' => 0,
-];
-// Test
-$titleTranslation = [
-    'Mr' => 36,
-    'Mme' => 35,
     'Mlle' => 42,
     '-' => 0,
 ];
